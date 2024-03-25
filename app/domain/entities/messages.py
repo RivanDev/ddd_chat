8745d0b79c218a80f36df2271e9b4e695b478e94
rivan <rivan_git@mail.ru>
2024-03-25 18:44:54 +0300

    add chat events, tests

--- app/domain/entities/messages.py
+++ app/domain/entities/messages.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass, field
 
 from domain.entities.base import BaseEntity
+from domain.events.messages import NewMessageReceivedEvent
 from domain.values.messages import Text, Title
 
 
-@dataclass(frozen=True)
+@dataclass(eq=False)
 class Message(BaseEntity):
     text: Text
 
 
-@dataclass(frozen=True)
+@dataclass(eq=False)
 class Chat(BaseEntity):
     title: Title
     messages: set[Message] = field(
@@ -17,5 +18,18 @@ class Chat(BaseEntity):
         kw_only=True,
     )
 
+    def __hash__(self) -> int:
+        return hash(self.oid)
+
+    def __eq__(self, __value: "Chat") -> bool:
+        return self.oid == __value.oid
+
     def add_message(self, message: Message):
         self.messages.add(message)
+        self.register_event(
+            NewMessageReceivedEvent(
+                message_text=message.text.as_generic_type(),
+                chat_oid=self.oid,
+                message_oid=message.oid,
+            )
+        )
