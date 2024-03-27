eeac3ce7858ccffccd9da7a3c6cc18f2801dfff0
rivan <rivan_git@mail.ru>
2024-03-28 01:09:45 +0300

    added endpoint to create chat + fixes

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+from application.api.messages.handlers import router as message_router
+
 
 def create_app() -> FastAPI:
-    return FastAPI(title="FastApi chat", docs_url="/api/docs", debug=True)
+    app = FastAPI(title="FastApi chat", docs_url="/api/docs", debug=True)
+    app.include_router(message_router, prefix="/chat")
+    return app
